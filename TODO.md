ae4b567abf828f24eb7e1705d685c0609e2d0620
Paulo Eduardo Neves <pauloneves@gmail.com>
2019-03-27 10:21:16 -0300

    Creating a service to get Album Data

---
+++ TODO.md
@@ -0,0 +1,1 @@
+- [ ] fiz até o 5 [desta página](https://app.pluralsight.com/projects/build-an-album-store-product-page-with-angular/creating-a-service-to-get-album-data)
---
+++ src/app/product.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+
+@Injectable()
+export class ProductService {
+  constructor(private _http: Http) {}
+}
